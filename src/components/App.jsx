3dad89d32cfee4b61f6b4026c45ccf6882c31a36
AifuwaRoland <siroapple10@gmail.com>
2021-01-02 20:11:25 -0600

    note  component with props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
+
+
+function createNote(notes){
+    return(
+        <Note
+        id={notes.id}
+        title={notes.title}
+        content={notes.content}
+        />
+    );
+}
 
 function App() {
     return (
     <div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
 
 
