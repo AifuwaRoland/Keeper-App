4789851f6b73868a592118d453e7388218c750bc
AifuwaRoland <siroapple10@gmail.com>
2021-01-05 21:51:30 -0600

    add note to array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,37 @@
-import React from "react";
-import Footer from "./Footer";
+import React, { useState } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
+import CreateArea from "./CreateArea";
 
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+
+
+    function addNote(newNote) { //arry with new note 
+
+        setNotes(prevNotes => { // get previous note 
+            return [...prevNotes, newNote]; // add new note  at the end
+        
+        })
+    }
     return (
         <div>
             <Header />
-            {notes.map((notes) => (
-                <Note
-                    id={notes.id}
-                    title={notes.title}
-                    content={notes.content}
-                />
+            <CreateArea
+                onAdd={addNote}
+            />
+            {notes.map(noteItem => { //loop through notes array 
 
-            ))}
+                return <Note title={noteItem.title}
+                    content={noteItem.content} />
+            })}
 
             <Footer />
-
-
         </div>
     );
 }
+
 export default App;
-
-
-
